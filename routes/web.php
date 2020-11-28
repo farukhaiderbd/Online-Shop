d7831bb9e1a2f9508c78bb12995f26a6c883ed93
farukhaidarbd <farukhaidar3@gmail.com>
2020-11-28 21:28:16 +0600

    SSLEcommerce

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SslCommerzPaymentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,9 +24,24 @@ Route::get('/', function () {
 
 
 Auth::routes();
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
+
 //website routes start
 Route::get('/', 'WebsiteController@index')->name('');
-
 Route::get('cart/product/view/{id}','CartController@ViewProduct');
 Route::get('/cart','CartController@showcart')->name('product_cart');
 Route::get('/checkout','CartController@checkout')->name('product_checkout');
